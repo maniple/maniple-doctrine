f78f505b83a009e1e37780b6aea9241ef31b1782
xemlock <952555+xemlock@users.noreply.github.com>
2020-04-07 11:16:41 +0200

    CLI: Fix handling of arguments passed to doctrine-cli

--- library/ManipleDoctrine/Tool/Provider/Doctrine.php
+++ library/ManipleDoctrine/Tool/Provider/Doctrine.php
@@ -5,10 +5,29 @@ namespace ManipleDoctrine\Tool\Provider;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
+/**
+ * Run doctrine cli, e.g.:
+ *
+ * maniple run doctrine orm:generate-proxies
+ *
+ * To pass additional options you have to separate them from command arg
+ * with '--', e.g.:
+ *
+ * maniple run doctrine orm:schema-tool:create -- --dump-sql
+ */
 class Doctrine extends \Maniple_Tool_Provider_Abstract
 {
-    public function runAction($command = null, $a1 = null, $b1 = null)
+    public function runAction($command = null)
     {
+        /** @var \Zefram_Tool_Framework_Client_Console $client */
+        $client = $this->_registry->getClient();
+        if (!$client instanceof \Zefram_Tool_Framework_Client_Console) {
+            throw new \Zend_Tool_Framework_Provider_Exception(sprintf(
+                'Client is expected to be an instance of Zefram_Tool_Framework_Client_Console, %s was provided.',
+                is_object($client) ? get_class($client) : gettype($client)
+            ));
+        }
+
         $application = $this->_getApplication()->bootstrap();
 
         /** @var EntityManager $entityManager */
@@ -16,7 +35,10 @@ class Doctrine extends \Maniple_Tool_Provider_Abstract
 
         $helperSet = ConsoleRunner::createHelperSet($entityManager);
 
-        $_SERVER['argv'] = array_merge(array('doctrine'), func_get_args());
+        $_SERVER['argv'] = array_merge(
+            array('doctrine', $command),
+            $client->getArgumentParser()->getRemainingArgs()
+        );
         $_SERVER['argc'] = count($_SERVER['argv']);
 
         ConsoleRunner::run($helperSet, array());
